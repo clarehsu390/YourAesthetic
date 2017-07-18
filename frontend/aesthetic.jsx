66bdc8d9c2f5755f3df261df7bf545140f932111
Clare Hsu <clare.hsu390@gmail.com>
2017-07-18 10:37:57 -0700

    Added configureStore

--- frontend/aesthetic.jsx
+++ frontend/aesthetic.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store.js';
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById("root");
-  ReactDOM.render(<h1>This is Your Aesthetic</h1>, root);
+  const store = configureStore();
+  ReactDOM.render(<Root store={store} />, root);
 });
---
+++ frontend/components/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+  <header>
+    <h1>Your Aesthetic</h1>
+  </header>
+);
+
+export default App;
