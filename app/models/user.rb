10de2389af7828b6345bee271ef639e132386683
Clare Hsu <clare.hsu390@gmail.com>
2017-07-17 18:47:22 -0700

    Updated User#is_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class User < ApplicationRecord
   end
 
   def is_password?(password)
-    BCrypt::Password.new(self.password_digest)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
   end
 
   def reset_session_token!
