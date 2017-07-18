46e83b38c921f01e787b3b13ac6c63cc6dab1922
Clare Hsu <clare.hsu390@gmail.com>
2017-07-18 10:20:38 -0700

    Add session actions

---
+++ frontend/aesthetic.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+  ReactDOM.render(<h1>Your Aesthetic</h1>, root);
+});
