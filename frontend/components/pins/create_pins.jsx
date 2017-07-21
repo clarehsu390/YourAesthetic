6454aca74df5b8846f5bcd36bd143e49fc3be890
Clare Hsu <clare.hsu390@gmail.com>
2017-07-21 13:34:26 -0700

    Created create pin

--- frontend/components/pins/create_pins.jsx
+++ frontend/components/pins/create_pins.jsx
@@ -1,3 +1,20 @@
 import React from 'react';
 import Dropzone from 'react-dropzone';
 import request from 'superagent';
+
+class CreatePin extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+    <Dropzone
+      multiple={false}
+      accept="image/*"
+      onDrop={this.onImageDrop.bind(this)}>
+      <p>Drop an image or click to select a file to upload.</p>
+    </Dropzone>
+  );
+  }
+}
